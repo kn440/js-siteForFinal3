4bf0c2d67da48d0cc31969b988c0c46dd733eb11
kn440 <78298987+kn440@users.noreply.github.com>
2024-08-07 00:48:08 +0300

    Add files via upload

---
+++ scriptblog.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: '#app2',
+  });
+Vue.component("blogtext", {
+    template:` 
+             <p>askjnkjasfkjhsjfhs</p>
+          `,
+  
+  
+    data() {
+      return {
+        
+      }
+    },
+  
+   
+  
+  });
+
+  
